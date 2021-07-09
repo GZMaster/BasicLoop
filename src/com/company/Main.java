0616038e2e53e588b905521421e2cb40c91f5a30
GZMaster <danigamester@gmail.com>
2021-07-09 21:53:08 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        for loop & while loop
+
+        int i = 0;
+
+        while (i < 5) {
+            System.out.println(" counting..." + i);
+            i++;
+        }
+
+//        for (int i = 0; i < 20; i++) {
+//
+//            //remainder %
+//            if (i % 3 == 0){
+//                System.out.println(i + " is a multiple of 3");
+//            }
+//
+//
+//        }
+
+
+
+    }
+}
